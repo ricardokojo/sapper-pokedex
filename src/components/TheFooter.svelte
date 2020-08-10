9f1e7a47f228d9cd9414bc8df47b5501df2a9206
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 14:54:18 -0300

    Create footer component
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

---
+++ src/components/TheFooter.svelte
@@ -0,0 +1,13 @@
+<style>
+.semiCircle {
+  position: relative;
+  background: #fefefe;
+  height: 15vh;
+
+  box-shadow: 0 0 1rem .1rem #333;
+  background:radial-gradient(circle at 50% 0%, #333 100px, #fefefe 101px);
+}
+</style>
+
+<footer class="semiCircle">
+</footer>
