31b471f7a1c6947b95af10ac2620f7a15149becc
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 18:34:17 -0300

    Create Sprites component
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

---
+++ src/components/Sprites.svelte
@@ -0,0 +1,124 @@
+<script>
+  export let pokemon;
+
+  $: hasGenderDifference =
+    pokemon.sprites.front_female || pokemon.sprites.back_female || pokemon.sprites.front_shiny_female || pokemon.sprites.back_shiny_female;
+</script>
+
+<style>
+  table {
+    background-color: #ddd;
+    border: 1px solid #666;
+    border-collapse: separate;
+    border-left: 0;
+    border-radius: 4px;
+    border-spacing: 0px;
+    table-layout: fixed;
+    width: 100%;
+  }
+
+  thead {
+    display: table-header-group;
+    vertical-align: middle;
+    border-color: inherit;
+    border-collapse: separate;
+  }
+
+  tr {
+    display: table-row;
+    vertical-align: inherit;
+    border-color: inherit;
+  }
+
+  th, td {
+    padding: 5px 4px 6px 4px;
+    text-align: left;
+    vertical-align: top;
+    border-left: 1px solid #666;
+  }
+
+  td {
+    border-top: 1px solid #666;
+    text-align: center;
+    vertical-align: center;
+  }
+
+  th {
+    text-align: center;
+    vertical-align: center;
+  }
+
+  thead:first-child tr:first-child th:first-child, tbody:first-child tr:first-child td:first-child {
+    border-radius: 4px 0 0 0;
+  }
+
+  thead:last-child tr:last-child th:first-child, tbody:last-child tr:last-child td:first-child {
+    border-radius: 0 0 0 4px;
+  }
+</style>
+
+{#if !hasGenderDifference}
+  <table>
+    <thead>
+      <tr>
+        <th>Front Sprite</th>
+        <th>Back Sprite</th>
+        <th>Shiny Front Sprite</th>
+        <th>Shiny Back Sprite</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><img src={pokemon.sprites.front_default} alt={`${pokemon.name}'s default front sprite`}></td>
+        <td><img src={pokemon.sprites.back_default} alt={`${pokemon.name}'s default back sprite`}></td>
+        <td><img src={pokemon.sprites.front_shiny} alt={`${pokemon.name}'s shiny front sprite`}></td>
+        <td><img src={pokemon.sprites.back_shiny} alt={`${pokemon.name}'s shiny back sprite`}></td>
+      </tr>
+    </tbody>
+  </table>
+{:else}
+  <table>
+    <thead>
+      <tr>
+        <th>Front Sprite - Female</th>
+        <th>Back Sprite - Female</th>
+        <th>Shiny Front Sprite - Female</th>
+        <th>Shiny Back Sprite - Female</th>
+        <th>Front Sprite - Male</th>
+        <th>Back Sprite - Male</th>
+        <th>Shiny Front Sprite - Male</th>
+        <th>Shiny Back Sprite - Male</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <img src={pokemon.sprites.front_female} alt={`${pokemon.name}'s female default front sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.back_female} alt={`${pokemon.name}'s female default back sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.front_shiny_female} alt={`${pokemon.name}'s female shiny front sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.back_shiny_female} alt={`${pokemon.name}'s female shiny back sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.front_default} alt={`${pokemon.name}'s male default front sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.back_default} alt={`${pokemon.name}'s male default back sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.front_shiny} alt={`${pokemon.name}'s male shiny front sprite`}>
+        </td>
+        <td>
+          <img src={pokemon.sprites.back_shiny} alt={`${pokemon.name}'s male shiny back sprite`}>
+        </td>
+      </tr>
+    </tbody>
+
+  </table>
+{/if}
+
