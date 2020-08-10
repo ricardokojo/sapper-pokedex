252a196bd92ed66a551af6e1ddc4fd4688a2f9c4
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 19:33:46 -0300

    Create progress bar like component
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

---
+++ src/components/StatBar.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let statValue;
+
+  $: statBasedWidth = (statValue / 255) * 100;
+  $: statBasedClass = statValue >= 100 ? 'high' : statValue >= 60 ? 'average' : 'low';
+</script>
+
+<style>
+.stat {
+  background-color: #fefefe;
+  border-radius: 5px;
+  height: 2rem;
+  width: 100%;
+
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+}
+
+.stat-bar {
+  border-radius: 5px;
+  height: inherit;
+}
+
+.low {
+  background-color: #f93131;
+  border: 1px solid #ee2b2b;
+}
+
+.average {
+  background-color: #ffdd57;
+  border: 1px solid #ebcb4b;
+}
+
+.high {
+  background-color: #06ae06;
+  border: 1px solid #069d06;
+}
+</style>
+
+<div class="stat">
+  <div class={`stat-bar ${statBasedClass}`} role="progressbar" style={`width: ${statBasedWidth}%`}>
+  </div>
+</div>
