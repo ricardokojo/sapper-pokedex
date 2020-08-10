312fcb6aa1c31d550634303dfe010a967b690755
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 20:00:00 -0300

    Add GitHub reference to footer
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/components/TheFooter.svelte
+++ src/components/TheFooter.svelte
@@ -1,13 +1,34 @@
 <style>
+footer {
+  display: flex;
+  flex-direction:  column;
+  align-items: center;
+  justify-content: flex-end;
+}
+
 .semi-circle {
   position: relative;
   background: #fefefe;
-  height: 15vh;
+  height: 20vh;
 
   box-shadow: 0 0 1rem .1rem #333;
   background:radial-gradient(circle at 50% 0%, #333 100px, #fefefe 101px);
 }
+
+p {
+  background-color: #f0f0f0;
+  color: #8a8a8a;
+  margin: 0;
+  padding: .5rem 0;
+  text-align: center;
+  width: 100%;
+}
+
+img {
+  width: 1.25rem;
+}
 </style>
 
 <footer class="semi-circle">
+  <p>MIT Licensed. Check out the repository here: <a href="https://github.com/ricardokojo/sapper-test"><img src="github.svg" alt="GitHub icon"/></a></p>
 </footer>
