9e30baaa6660bdcee7224b49b781d30ef318a556
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 14:57:23 -0300

    Update default layout to use new header and footer components
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,23 @@
 <script>
-  import Nav from '../components/Nav.svelte';
-
-  export let segment;
+  import TheHeader from '../components/TheHeader.svelte';
+  import TheFooter from '../components/TheFooter.svelte';
 </script>
 
 <style>
   main {
     position: relative;
     max-width: 56em;
-    background-color: white;
+    height: 100%;
     padding: 2em;
-    margin: 0 auto;
+    margin: 4rem auto;
     box-sizing: border-box;
   }
 </style>
 
-<Nav {segment}/>
+<TheHeader />
 
 <main>
   <slot></slot>
 </main>
+
+<TheFooter />
