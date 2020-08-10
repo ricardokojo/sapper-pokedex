142d468eb72af83f52c4050a5e4ee1f4c08eaff3
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 19:35:31 -0300

    Update number component to use new components
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/routes/pokemon/[number].svelte
+++ src/routes/pokemon/[number].svelte
@@ -10,11 +10,16 @@
 
 <script>
   import BasicInfo from '../../components/BasicInfo.svelte';
+  import Stats from '../../components/Stats.svelte';
+  import Sprites from '../../components/Sprites.svelte'
 
   export let pokemon;
+  const lastPokemonNumber = 807; // PokeAPI contains information up to Zeraora (807)
   console.log(pokemon);
 
   $: paddedNumber = pokemon.id.toString().padStart(3, "0");
+  $: previousPokemonNumber = pokemon.id - 1;
+  $: nextPokemonNumber = pokemon.id + 1;
 </script>
 
 <style>
@@ -37,6 +42,18 @@
   h1 > span {
     text-transform: capitalize;
   }
+
+  .bottom-links {
+    display: flex;
+  }
+
+  .bottom-links .previous {
+    margin-right: auto;
+  }
+
+  .bottom-links .next {
+    margin-left: auto;
+  }
 </style>
 
 <svelte:head>
@@ -47,17 +64,17 @@
 
 <h1>{paddedNumber} — <span>{pokemon.name}</span></h1>
 
-<BasicInfo pokemon={pokemon} />
+<BasicInfo pokemon={pokemon}/>
 
-<div class=''>
-  <ul>
-    {#each pokemon.stats as stat}
-      <li>{stat.base_stat}</li>
-    {/each}
-  </ul>
+<Stats pokemon={pokemon}/>
 
-  {#each Object.entries(pokemon.sprites) as [key, value]}
-    {key}
-    <img src={value}/>
-  {/each}
+<Sprites pokemon={pokemon}/>
+
+<div class="bottom-links">
+  {#if previousPokemonNumber > 0}
+    <a href={`/pokemon/${previousPokemonNumber}`} class="previous">‹ Anterior</a>
+  {/if}
+  {#if nextPokemonNumber <= lastPokemonNumber}
+    <a href={`/pokemon/${nextPokemonNumber}`} class="next">Próximo ›</a>
+  {/if}
 </div>
