87094fad4962eaf3371bb3ebb6438fa677220d7a
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 15:36:39 -0300

    Update index to display box sprites
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,34 @@
 </script>
 
 <style>
+.pokemon-list {
+  display: flex;
+  flex-flow: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.pokemon-link {
+  box-shadow: none;
+  border-radius: 5px;
+  text-decoration: none;
+  transition: box-shadow 0.1s;
+}
+
+.pokemon-link:hover {
+  box-shadow: 0 0 1rem .1rem #333;
+}
 </style>
 
 <svelte:head>
   <title>Hackday 10/08 - Sapper Pokédex</title>
 </svelte:head>
 
-<ol>
+<div class="pokemon-list">
   {#each Array(151) as _, i}
-    <li>
-      <a href={`pokemon/${i + 1}`}>{i + 1}</a>
-    </li>
+    <a href={`pokemon/${i + 1}`} class="pokemon-link">
+      <img src={`box-sprites/${(i + 1).toString().padStart(3, "0")}.png`} alt={`Box sprite for Pokémon number ${i + 1}`}>
+    </a>
   {/each}
-</ol>
+</div>
+
