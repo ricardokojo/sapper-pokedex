17bf8eb4f6f358c97e4accc77f6ae24b95c317c1
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2020-08-10 15:36:15 -0300

    Add container behaviour to main tag
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,14 +4,37 @@
 </script>
 
 <style>
+main {
+  margin-top: 6rem;
+  margin-bottom: 4rem;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+
+@media (min-width: 640px) {
   main {
-    position: relative;
-    max-width: 56em;
-    height: 100%;
-    padding: 2em;
-    margin: 4rem auto;
-    box-sizing: border-box;
+    max-width: 640px;
   }
+}
+
+@media (min-width: 768px) {
+  main {
+    max-width: 768px;
+  }
+}
+
+@media (min-width: 1024px) {
+  main {
+    max-width: 1024px;
+  }
+}
+
+@media (min-width: 1280px) {
+  main {
+    max-width: 1280px;
+  }
+}
 </style>
 
 <TheHeader />
